8fe2d8acc613da4ec02ceca97b2c9d48d5547b58
jyoti ranjan sahoo <jyotiranjansahoo038@gmail.com>
2026-04-29 23:28:07 +0530

    initialization done

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+const page = () => {
+  return (
+    <>
+    <div className="flex bg-linear-to-r from-blue-500 to-purple-500 items-center justify-center h-screen">
+      <div className="bg-linear-to-r from-purple-500 to-blue-500 p-8  text-blue-200 text-3xl font-extrabold font-serif">Hello, Next.js!</div>
+    </div>
+    </>
+  )
+}
+
+export default page
