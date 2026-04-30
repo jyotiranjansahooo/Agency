988889504c5013c89799df2b3219826b5502f9de
jyoti ranjan sahoo <jyotiranjansahoo038@gmail.com>
2026-04-30 23:34:59 +0530

    add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ const page = () => {
   return (
     <>
     <div className="flex bg-linear-to-r from-blue-500 to-purple-500 items-center justify-center h-screen">
-      <div className="bg-linear-to-r from-purple-500 to-blue-500 p-8  text-blue-200 text-3xl font-extrabold font-serif">Hello, Next.js!</div>
+      <div className="bg-linear-to-r from-purple-500 to-blue-500 p-8  text-blue-200 text-3xl font-extrabold ">Hello, Next.js!</div>
     </div>
     </>
   )
